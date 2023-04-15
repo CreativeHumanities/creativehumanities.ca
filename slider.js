2813faddfbf532c850ad4cea2dc5e5e707c4b46f
CreativeHumanities <129673249+CreativeHumanities@users.noreply.github.com>
2023-04-15 14:36:57 -0400

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // Initialize the slick slider with dots enabled
+  $('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+  });
+});
